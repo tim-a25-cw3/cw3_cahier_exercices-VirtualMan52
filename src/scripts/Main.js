8840f9b01f426a290e42f9a720ec1c870fc40165
VirtualMan52 <virtu@virtualman52.com>
2025-11-12 15:49:36 -0500

    header hero et labos

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,10 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 class Main {
   constructor() {
@@ -7,6 +13,17 @@ class Main {
 
   init() {
     Icons.load();
+    this.initializeCarousel();
+  }
+
+  initializeCarousel() {
+    const swiper = new Swiper('.swiper-js', {
+      // configure Swiper to use modules
+      modules: [Pagination],
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
   }
 }
 new Main();
